a10e838f4cec76ea8c44a353a19bb6c687fd94d7
JoaoPedrosz <joaosilvajs013@gmail.com>
2025-10-02 22:08:57 -0300

    att

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,23 @@
-import { fastify } from "fastify"
+import { env } from "node:process";
+import { fastifyCors } from "@fastify/cors";
+import { fastify } from "fastify";
 import {
-    serializerCompiler,
-    validatorCompiler,
-    ZodTypeProvider,
-} from "fastify-type-provider-zod"   
-import { fastifyCors} from "@fastify/cors"
+	serializerCompiler,
+	validatorCompiler,
+	type ZodTypeProvider,
+} from "fastify-type-provider-zod";
 
-const app = fastify()
+const app = fastify().withTypeProvider<ZodTypeProvider>();
 
 app.register(fastifyCors, {
-    origin: "http://localhost:5173",
-})  
+	origin: "http://localhost:5173",
+});
+
+app.setSerializerCompiler(serializerCompiler);
+app.setValidatorCompiler(validatorCompiler);
+
+app.get("/health", async () => {
+	return { status: "OK" };
+});
+
+app.listen({ port: env.PORT });
